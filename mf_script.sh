4871da098a6a4c9d77e1c71fe2a31338562d9547
Mads Jensen <mje.mads@gmail.com>
2015-08-24 11:39:50 +0200

    fixed typo

--- mf_script.sh
+++ mf_script.sh
@@ -1,6 +1,6 @@
 
 
-maxfilter -f p_01_data.fif -o p_02_data_raw_tsss1.fif -st 30 -corr 0.95 -autobad on -movecomp -hp p_01_data_tsss_hp.txt -v > p_01_data_tsss.log
+maxfilter -f p_01_data.fif -o p_02_data_raw_tsss.fif -st 30 -corr 0.95 -autobad on -movecomp -hp p_01_data_tsss_hp.txt -v > p_01_data_tsss.log
 
 maxfilter -f p_01_data-1.fif -o p_02_data_raw_tsss-1.fif -st 30 -corr 0.95 -autobad on -movecomp -hp p_01_data_tsss_hp-1.txt -v > p_01_data_tsss-1.log
 
