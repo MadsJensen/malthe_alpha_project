e92291b65bfb3d961802e797a4d2f526abe3bdc7
Mads Jensen <mje.mads@gmail.com>
2015-09-01 11:00:11 +0200

    updated

--- cal_inverse_extract_ts.py
+++ cal_inverse_extract_ts.py
@@ -33,7 +33,7 @@ fname_epochs = data_path + 'p_01_ica_filter_ds_tsss-epo.fif'
 snr = 1.0  # Standard assumption for average data but using it for single trial
 lambda2 = 1.0 / snr ** 2
 
-method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
+method = "MNE"  # use dSPM method (could also be MNE or sLORETA)
 
 # Load data
 inverse_operator = read_inverse_operator(fname_inv)
@@ -52,3 +52,53 @@ for cond in epochs.event_id.keys():
                                 pick_ori="normal")
     exec("stcs_%s = stcs" % cond)
 
+
+from mne.minimum_norm import read_inverse_operator, source_induced_power
+
+# Compute a source estimate per frequency band including and excluding the
+# evoked response
+frequencies = np.arange(7, 16, 1)  # define frequencies of interest
+n_cycles = frequencies / 3.  # different number of cycle per frequency
+
+# subtract the evoked response in order to exclude evoked activity
+epochs_induced = epochs["ent_L", "ent_R" ].copy().subtract_evoked()
+
+#plt.close('all')
+
+for ii, (this_epochs, title) in enumerate(zip([epochs["ent_L", "ent_R"],
+                                                epochs_induced],
+                                              ['evoked + induced',
+                                               'induced only'])):
+    # compute the source space power and phase lock
+    power, phase_lock = source_induced_power(
+        this_epochs, inverse_operator, frequencies, label,
+        baseline=(0.7, 0.95),
+        baseline_mode='zscore', n_cycles=n_cycles, n_jobs=n_jobs)
+
+    power = np.mean(power, axis=0)  # average over sources
+    phase_lock = np.mean(phase_lock, axis=0)  # average over sources
+    times = epochs.times
+
+    ##########################################################################
+    # View time-frequency plots
+    plt.subplots_adjust(0.1, 0.08, 0.96, 0.94, 0.2, 0.43)
+    plt.subplot(2, 2, 2 * ii + 1)
+    plt.imshow(20 * power,
+               extent=[times[60], times[260], frequencies[0], frequencies[-1]],
+               aspect='auto', origin='lower', cmap='RdBu_r')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Power (%s)' % title)
+    plt.colorbar()
+
+    plt.subplot(2, 2, 2 * ii + 2)
+    plt.imshow(phase_lock,
+               extent=[times[60], times[260], frequencies[0], frequencies[-1]],
+               aspect='auto', origin='lower',
+               cmap='RdBu_r')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.title('Phase-lock (%s)' % title)
+    plt.colorbar()
+
+plt.show()
