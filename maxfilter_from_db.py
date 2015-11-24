fc4772fb1b216cfb552d01c88f984206a6072043
Mads Jensen <mje.mads@gmail.com>
2015-11-24 11:13:28 +0100

    Updated experiment name and paths
    plus test for 1 sub

--- maxfilter_from_db.py
+++ maxfilter_from_db.py
@@ -1,4 +1,6 @@
 """
+Doc string here.
+
 @author: cjb
 @author: mje
 
@@ -6,9 +8,10 @@ last edited: Wed Sep 9 2015
 """
 
 from __future__ import print_function
-import os, sys
-import subprocess
-import numpy as np
+import os
+import sys
+# import subprocess
+# import numpy as np
 from mne.preprocessing.maxfilter import apply_maxfilter
 
 CLOBBER = False
@@ -42,18 +45,18 @@ from stormdb.access import Query
 #                         )
 
 
-proj_code = "MEG_EEG-Training"
+proj_code = "MINDLAB2015_MEG-CorticalAlphaAttention"
 
 db = Query(proj_code)
 proj_folder = os.path.join('/projects', proj_code)
-scratch_folder = os.path.join(proj_folder, 'scratch/mje_maxfilter_test/')
+scratch_folder = os.path.join(proj_folder, 'scratch/maxfiltered')
 
 subjects_dir = os.path.join(scratch_folder, 'fs_subjects_dir')
 script_dir = proj_folder + '/scripts/'
 
 included_subjects = db.get_subjects()
 # just test with first one!
-included_subjects = included_subjects[:2]
+included_subjects = included_subjects[3]
 
 for sub in included_subjects:
     # this is an example of getting the DICOM files as a list
@@ -67,9 +70,9 @@ for sub in included_subjects:
         for serie in series:
             if serie[0] != "empty_room":
                 in_name = db.get_files(sub, MEG_study, 'MEG', serie[1])
-                out_name = "%s_%s-raw_tsss_mc_TEST.fif" % (sub[:4], serie[0])
+                out_name = "%s_%s_mc-raw_tsss.fif" % (sub[:4], serie[0])
                 print(out_name)
-    
+
                 # if len(in_name) > 1:
                 for j, in_file in enumerate(in_name):
                     if j == 0:
@@ -77,13 +80,13 @@ for sub in included_subjects:
                     else:
                         out_fname = scratch_folder\
                                     + out_name[:-4] + "-%d.fif" % j
-                                    
+
                     tsss_mc_log = out_fname[:-3] + "log"
                     headpos_log = out_fname[:-4] + "_hp.log"
-                    
+
                     print(tsss_mc_log)
                     print(headpos_log)
-                        
+
                     apply_maxfilter(in_fname=in_file,
                                     out_fname=out_fname,
                                     frame='head',
