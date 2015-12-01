ba40c2b7317da77fa58a42311f568c7e3eb9464e
Mads Jensen <mje.mads@gmail.com>
2015-12-01 12:08:39 +0100

    updated to use database more

--- maxfilter_from_db.py
+++ maxfilter_from_db.py
@@ -23,81 +23,88 @@ path_to_stormdb = '/usr/local/common/meeg-cfin/stormdb'
 sys.path.append(path_to_stormdb)
 
 from stormdb.access import Query
+from stormdb.process import Maxfilter
 
 
 # MAXFILTER PARAMS #
-
-#         #call to maxfilter
-#         apply_maxfilter(in_fname=in_name,
-#                         out_fname=out_name,
-#                         frame='head',
-# #                        origin= "0 0 40",
-#                         autobad="on",
-#                         st=True,
-#                         st_buflen=30,
-#                         st_corr=0.95,
-#                         mv_comp=True,
-#                         mv_hp=headpos_log,
-#                         cal='/projects/MINDLAB2011_24-MEG-readiness/misc/sss_cal_Mar11-May13.dat',
-#                         ctc='/projects/MINDLAB2011_24-MEG-readiness/misc/ct_sparse_Mar11-May13.fif',
-#                         overwrite=True,
-#                         mx_args=' -v | tee %s' % tsss_mc_log,
-#                         )
+mf_params = dict(origin='0 0 40',
+                 frame='head',
+                 logfile=tsss_mc_log,
+                 autobad="on",
+                 st=True,
+                 st_buflen=30,
+                 st_corr=0.95,
+                 movecomp=True,
+                 mv_hp=headpos_log,
+                 # cal=cal,
+                 # ctc=ctc,
+                 mx_args='',
+                 maxfilter_bin='maxfilter',
+                 force=True
+                 )
 
 
 proj_code = "MINDLAB2015_MEG-CorticalAlphaAttention"
 
 db = Query(proj_code)
 proj_folder = os.path.join('/projects', proj_code)
-scratch_folder = os.path.join(proj_folder, 'scratch/maxfiltered')
+scratch_folder = os.path.join(proj_folder, 'scratch/maxfiltered/')
 
 subjects_dir = os.path.join(scratch_folder, 'fs_subjects_dir')
 script_dir = proj_folder + '/scripts/'
 
 included_subjects = db.get_subjects()
 # just test with first one!
-included_subjects = included_subjects[3]
+included_subjects = included_subjects[3:]
 
 for sub in included_subjects:
     # this is an example of getting the DICOM files as a list
     # sequence_name='t1_mprage_3D_sag'
     MEG_study = db.get_studies(sub, modality='MEG')
-    if MEG_study is not None:
+    if len(MEG_study) > 0:
         # This is a 2D list with [series_name, series_number]
         series = db.get_series(sub, MEG_study[0], 'MEG')
         series = series["data"]  # only use the "data" series for now.
 
     # Change this to be more elegant: check whether any item in series
     # matches sequence_name
-    in_name = db.get_files(sub, MEG_study[0], 'MEG', series)
-    out_name = "%s_%s_mc-raw_tsss.fif" % (sub[:4], "data")
-    print(out_name)
-
-    # if len(in_name) > 1:
-    for j, in_file in enumerate(in_name):
-        if j == 0:
-            out_fname = scratch_folder + out_name
-        else:
-            out_fname = scratch_folder\
-                        + out_name[:-4] + "-%d.fif" % j
-
-        tsss_mc_log = out_fname[:-3] + "log"
-        headpos_log = out_fname[:-4] + "_hp.log"
-
-        print(tsss_mc_log)
-        print(headpos_log)
-
-        apply_maxfilter(in_fname=in_file,
-                        out_fname=out_fname,
-                        frame='head',
-                        # origin= "0 0 40",
-                        autobad="on",
-                        st=True,
-                        st_buflen=30,
-                        st_corr=0.95,
-                        mv_comp=True,
-                        mv_hp=headpos_log,
-                        # cal=cal,
-                        # ctc=ctc,
-                        overwrite=True,
-                        mx_args=' -v > %s' % tsss_mc_log)
+        in_name = db.get_files(sub, MEG_study[0], 'MEG', series)
+        out_name = "%s_%s_mc_raw_tsss.fif" % (sub[:4], "data")
+#        print(out_name)
+    
+        # if len(in_name) > 1:
+        for j, in_file in enumerate(in_name):
+            if j == 0:
+                out_fname = scratch_folder + out_name
+            else:
+                out_fname = scratch_folder\
+                            + out_name[:-4] + "-%d.fif" % j
+    
+            print(out_fname)
+            tsss_mc_log = out_fname[:-3] + "log"
+            headpos_log = out_fname[:-4] + "_hp.log"
+    
+            print(tsss_mc_log)
+            print(headpos_log)
+    
+            mf_params["logfile"] = tsss_mc_log
+            mf_params["mv_hp"] = headpos_log
+            mf=Maxfilter(proj_code)
+            mf.build_maxfilter_cmd(in_file, out_fname, **mf_params)
+            
+            print(mf.cmd)
+                    
+#        apply_maxfilter(in_fname=in_file,
+#                        out_fname=out_fname,
+#                        frame='head',
+#                        # origin= "0 0 40",
+#                        autobad="on",
+#                        st=True,
+#                        st_buflen=30,
+#                        st_corr=0.95,
+#                        mv_comp=True,
+#                        mv_hp=headpos_log,
+#                        # cal=cal,
+#                        # ctc=ctc,
+#                        overwrite=True,
+#                        mx_args=' -v > %s' % tsss_mc_log)
