ad44e680e6b0e5c73abff7a5708069c707374d92
Mads Jensen <mje.mads@gmail.com>
2015-12-07 13:19:20 +0100

    added filtered to save

--- filter_ICA.py
+++ filter_ICA.py
@@ -60,7 +60,7 @@ def filter_data(subject, l_freq=l_freq, h_freq=h_freq, n_freq=n_freq,
     raw.filter(l_freq, h_freq, n_jobs=n_jobs)
 
     if save is True:
-        raw.save(save_folder + "%s_data_mc_raw_tsss.fif" % subject)
+        raw.save(save_folder + "%s_filterd_data_mc_raw_tsss.fif" % subject)
 
 
 subjects = ["0004"]
