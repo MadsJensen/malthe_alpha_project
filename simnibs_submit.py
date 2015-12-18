ef73a9c76efbe38deed0c95fda3c6b41769a4496
Mads Jensen <mje.mads@gmail.com>
2015-12-18 09:28:39 +0100

    commented out some to rerun from term

--- simnibs_submit.py
+++ simnibs_submit.py
@@ -40,37 +40,38 @@ script_dir = proj_folder + '/scripts/'
 
 included_subjects = db.get_subjects()
 # just test with first one!
-included_subjects = included_subjects[5:]
+#included_subjects = included_subjects[5:]
 
-for sub in included_subjects:
-    # this is an example of getting the DICOM files as a list
-    # sequence_name='t1_mprage_3D_sag'
-    MR_study = db.get_studies(sub, modality='MR')
-    if len(MR_study) > 0:
-        # This is a 2D list with [series_name, series_number]
-        series = db.get_series(sub, MR_study[0], 'MR')
-        t1 = series["t1_mpr_sag_weakFS"]  # only use the "data" series for now.
-        t2 = series["t2_tse_sag_HighBW"]
-        
-    # Change this to be more elegant: check whether any item in series
-    # matches sequence_name
-        in_t1 = db.get_files(sub, MR_study[0], 'MR', t1)
-        in_t2 = db.get_files(sub, MR_study[0], 'MR', t2)
-
-        subj_fname = sub[:4]
-        # print(out_name)
-
-        convert_t1 = "mri_convert %s %s_t1.nii.gz"  % (in_t1[0], subj_fname)
-        print(convert_t1)
-        convert_t2 = "mri_convert %s %s_t2.nii.gz"  % (in_t2[0], subj_fname)
-        print(convert_t2)
-                
-        os.chdir(fs_subjects_dir + subj_fname)
-        subprocess.call([cmd, "1", convert_t1])
-        subprocess.call([cmd, "1", convert_t2])
+#for sub in included_subjects:
+#    # this is an example of getting the DICOM files as a list
+#    # sequence_name='t1_mprage_3D_sag'
+#    MR_study = db.get_studies(sub, modality='MR')
+#    if len(MR_study) > 0:
+#        # This is a 2D list with [series_name, series_number]
+#        series = db.get_series(sub, MR_study[0], 'MR')
+#        t1 = series["t1_mpr_sag_weakFS"]  # only use the "data" series for now.
+#        t2 = series["t2_tse_sag_HighBW"]
+#        
+#    # Change this to be more elegant: check whether any item in series
+#    # matches sequence_name
+#        in_t1 = db.get_files(sub, MR_study[0], 'MR', t1)
+#        in_t2 = db.get_files(sub, MR_study[0], 'MR', t2)
+#
+#        subj_fname = sub[:4]
+#        # print(out_name)
+#
+#        convert_t1 = "mri_convert %s %s_t1.nii.gz"  % (in_t1[0], subj_fname)
+#        print(convert_t1)
+#        convert_t2 = "mri_convert %s %s_t2.nii.gz"  % (in_t2[0], subj_fname)
+#        print(convert_t2)
+#                
+#        os.chdir(fs_subjects_dir + subj_fname)
+#        subprocess.call([cmd, "1", convert_t1])
+#        subprocess.call([cmd, "1", convert_t2])
     
     
-included_subjects =[u'0006_O7K',
+included_subjects =['0005_XXX',
+                    u'0006_O7K',
                      u'0007_AGN',
                      u'0008_PDM',
                      u'0009_8PP',
