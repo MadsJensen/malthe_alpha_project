e24d058f0f56296b35d19c64fe45b590de5832e7
Mads Jensen <mje.mads@gmail.com>
2016-01-29 00:14:20 +0100

    syntax fixes

--- fwd_inv.py
+++ fwd_inv.py
@@ -16,7 +16,7 @@ hostname = socket.gethostname()
 if hostname == "wintermute":
     data_path = "/home/mje/mnt/caa/scratch/"
 else:
-    data_path = "projects/MINDLAB2015_MEG-CorticalAlphaAttention/scratch/"
+    data_path = "/projects/MINDLAB2015_MEG-CorticalAlphaAttention/scratch/"
 
 # CHANGE DIR TO SAVE FILES THE RIGTH PLACE
 os.chdir(data_path)
@@ -29,18 +29,25 @@ tf_folder = data_path + "tf_data/"
 mne_folder = data_path + "minimum_norm/"
 
 
-subjects = ["0004", "0005"]  # subject to run
+subjects = ["0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011",
+            "0012", "0013", "0014", "0015", "0016", "0017", "0020", "0021",
+            "0022", "0023", "0024", "0025"]  # subject to run
+
+os.chdir(mne_folder)
+bem_list = !ls *8192-8192*sol.fif
+
+subjects = ["0004"]
 
 # Setup source space and forward model
-for sub in subjects:
+for j, sub in enumerate(subjects):    
     raw_fname = save_folder + "%s_filtered_ica_mc_raw_tsss.fif" % sub
     trans_fname = mne_folder + "%s-trn-trans.fif" % sub
-    bem = mne.read_bem_solution(mne_folder +
-                                "%s-8192-8192-*-bem-sol.fif" % sub)
+    bem = bem_list[j]
     cov = mne.read_cov(mne_folder + "%s-cov.fif" % sub)
-    cov = cov[0]
+#    cov = cov[0]
 
-    src = mne.setup_source_space("%s-oct6-src.fif" % sub,
+    src = mne.setup_source_space(sub,
+                                 mne_folder + "%s-oct6-src.fif" % sub,
                                  spacing="oct6",
                                  subjects_dir=subjects_dir,
                                  n_jobs=2)  # use a job for each hemispere
@@ -55,9 +62,9 @@ for sub in subjects:
 
 # Calculate covariance matrix
 for sub in subjects:
-    epochs = mne.read_epochs("%s_filtered_ica_mc_tsss-epo.fif" % sub)
+    epochs = mne.read_epochs(epochs_folder + "%s_filtered_ica_mc_tsss-epo.fif" % sub)
     cov = mne.compute_covariance(epochs, tmin=None, tmax=-0.01,
-                                 method="auto")
+                                 method="auto", return_estimators="all")
     cov.save(mne_folder + "%s-cov.fif" % sub)
 
 
@@ -65,7 +72,8 @@ for sub in subjects:
 for sub in subjects:
     fwd = mne.read_forward_solution(mne_folder + "%s-fwd.fif" % sub)
     cov = mne.read_cov(mne_folder + "%s-cov.fif" % sub)
-    epochs = mne.read_epochs("%s_filtered_ica_mc_tsss-epo.fif" % sub)
+    epochs = mne.read_epochs(epochs_folder +\
+                            "%s_filtered_ica_mc_tsss-epo.fif" % sub)
     inv = make_inverse_operator(epochs.info, fwd, cov,
                                 loose=0.2, depth=0.8)
 
