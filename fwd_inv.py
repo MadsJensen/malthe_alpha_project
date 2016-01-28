e4ef8ac85669463ef2201eb326d025bdf1d90679
Mads Jensen <mje.mads@gmail.com>
2016-01-29 00:19:36 +0100

    PEP fixes

--- fwd_inv.py
+++ fwd_inv.py
@@ -34,12 +34,12 @@ subjects = ["0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011",
             "0022", "0023", "0024", "0025"]  # subject to run
 
 os.chdir(mne_folder)
-bem_list = !ls *8192-8192*sol.fif
+bem_list = !ls * 8192-8192*sol.fif
 
 subjects = ["0004"]
 
 # Setup source space and forward model
-for j, sub in enumerate(subjects):    
+for j, sub in enumerate(subjects):
     raw_fname = save_folder + "%s_filtered_ica_mc_raw_tsss.fif" % sub
     trans_fname = mne_folder + "%s-trn-trans.fif" % sub
     bem = bem_list[j]
@@ -62,7 +62,8 @@ for j, sub in enumerate(subjects):
 
 # Calculate covariance matrix
 for sub in subjects:
-    epochs = mne.read_epochs(epochs_folder + "%s_filtered_ica_mc_tsss-epo.fif" % sub)
+    epochs = mne.read_epochs(epochs_folder +
+                             "%s_filtered_ica_mc_tsss-epo.fif" % sub)
     cov = mne.compute_covariance(epochs, tmin=None, tmax=-0.01,
                                  method="auto", return_estimators="all")
     cov.save(mne_folder + "%s-cov.fif" % sub)
@@ -72,8 +73,8 @@ for sub in subjects:
 for sub in subjects:
     fwd = mne.read_forward_solution(mne_folder + "%s-fwd.fif" % sub)
     cov = mne.read_cov(mne_folder + "%s-cov.fif" % sub)
-    epochs = mne.read_epochs(epochs_folder +\
-                            "%s_filtered_ica_mc_tsss-epo.fif" % sub)
+    epochs = mne.read_epochs(epochs_folder +
+                             "%s_filtered_ica_mc_tsss-epo.fif" % sub)
     inv = make_inverse_operator(epochs.info, fwd, cov,
                                 loose=0.2, depth=0.8)
 
