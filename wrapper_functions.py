a92283c74ef4bcbe7849e6522d8d12ea0bb136de
Mads Jensen <mje.mads@gmail.com>
2016-01-30 13:43:45 +0100

    Initial commit for wrapper function

---
+++ wrapper_functions.py
@@ -0,0 +1,36 @@
+"""
+Doc string here.
+
+@author mje
+@email: mads [] cnru.dk
+
+"""
+import os
+import sys
+import subprocess
+
+cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
+
+# SETUP PATHS AND PREPARE RAW DATA
+hostname = socket.gethostname()
+
+if hostname == "wintermute":
+    data_path = "/home/mje/mnt/caa/scratch/"
+    script_path = "/home/mje/mnt/caa/scripts/"
+else:
+    data_path = "/projects/MINDLAB2015_MEG-CorticalAlphaAttention/scratch/"
+    script_path = "/projects/MINDLAB2015_MEG-CorticalAlphaAttention/scripts/"
+
+# CHANGE DIR TO SAVE FILES THE RIGTH PLACE
+os.chdir(script_path)
+mne_folder = data_path + "minimum_norm/"
+
+
+subjects = ["0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011",
+            "0012", "0013", "0014", "0015", "0016", "0017", "0020", "0021",
+            "0022", "0023", "0024", "0025"]  # subject to run
+
+
+for subject in subjects[2:]:
+    submit_cmd = "python submit_calc_cov %s" % subject
+    subprocess.call([cmd, "1", submit_cmd])
