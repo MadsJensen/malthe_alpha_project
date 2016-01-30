9a76bf66c32161101ae5671b9888fc472691c8af
Mads Jensen <mje.mads@gmail.com>
2016-01-30 13:33:13 +0100

    Initial commit for function to submit to isis SGE

---
+++ submit_calc_cov.py
@@ -0,0 +1,39 @@
+"""
+Doc string goes here.
+
+@author: mje mads [] cnru.dk
+"""
+import socket
+import mne
+import os
+import sys
+
+cmd = "/usr/local/common/meeg-cfin/configurations/bin/submit_to_isis"
+
+# SETUP PATHS AND PREPARE RAW DATA
+hostname = socket.gethostname()
+
+if hostname == "wintermute":
+    data_path = "/home/mje/mnt/caa/scratch/"
+else:
+    data_path = "/projects/MINDLAB2015_MEG-CorticalAlphaAttention/scratch/"
+
+# CHANGE DIR TO SAVE FILES THE RIGTH PLACE
+subjects_dir = data_path + "fs_subjects_dir/"
+save_folder = data_path + "filter_ica_data/"
+maxfiltered_folder = data_path + "maxfiltered_data/"
+epochs_folder = data_path + "epoched_data/"
+tf_folder = data_path + "tf_data/"
+mne_folder = data_path + "minimum_norm/"
+
+os.chdir(mne_folder)
+
+# import subject from commandline arg
+sub = str(sys.argv[1])
+
+
+epochs = mne.read_epochs(epochs_folder +
+                         "%s_filtered_ica_mc_tsss-epo.fif" % sub)
+cov = mne.compute_covariance(epochs, tmin=None, tmax=-0.01,
+                             method="factor_analysis")
+cov.save(mne_folder + "%s-cov.fif" % sub)
