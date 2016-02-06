a3d5feb1a72a2887ef9257547f173d1ef649445a
Mads Jensen <mje.mads@gmail.com>
2016-02-06 21:22:56 +0100

    Updates

--- fwd_inv.py
+++ fwd_inv.py
@@ -41,23 +41,23 @@ os.chdir(mne_folder)
 bem_list = glob.glob("*8192-8192*sol.fif")
 bem_list.sort()
 
-subjects = ["0004", "0005"]
+subjects = ["0006"]
 
 # Setup source space and forward model
 for j, subject in enumerate(subjects[1:11]):
-    print("Workingt on subject: %s" % subject )
-    
+    print("Workingt on subject: %s" % subject)
+
     raw_fname = save_folder + "%s_filtered_ica_mc_raw_tsss.fif" % subject
     trans_fname = mne_folder + "%s-trans.fif" % subject
     bem = bem_list[j]
     cov = mne.read_cov(mne_folder + "%s-cov.fif" % subject)
 
     src = mne.setup_source_space(subject,
-                                 mne_folder + "%s-oct6-src.fif" % subject,
-                                 spacing="oct6",
+                                 mne_folder + "%s-all-src.fif" % subject,
+                                 spacing="all",
                                  subjects_dir=subjects_dir,
                                  n_jobs=2,
-                                 overwrite=True)  # use a job for each hemispere
+                                 overwrite=True)  # 1 for each hemispere
 
     fwd = mne.make_forward_solution(raw_fname, trans=trans_fname,
                                     src=src,
@@ -85,10 +85,20 @@ for subject in subjects:
     fwd = mne.read_forward_solution(mne_folder + "%s-fwd.fif" % subject)
     cov = mne.read_cov(mne_folder + "%s-cov.fif" % subject)
     epochs = mne.read_epochs(epochs_folder +
-                             "%s_filtered_ica_mc_tsss-epo.fif" % subject)
+                             "%s_filtered_ica_mc_tsss-epo.fif" % subject,
+                             preload=False)
     inv = make_inverse_operator(epochs.info, fwd, cov,
                                 loose=0.2, depth=0.8)
 
     mne.minimum_norm.write_inverse_operator(mne_folder +
                                             "%s-inv.fif" % subject,
                                             inv)
+
+
+os.chdir(mne_folder)
+invs = glob.glob("*-inv.fif")
+invs.sort()
+
+for i in invs[:10]:
+    tmp = mne.minimum_norm.read_inverse_operator(i)
+    print(tmp)
