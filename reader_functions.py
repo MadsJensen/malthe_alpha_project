ad6d5d9556d9e9fa1b85d1be33f83af0a1da0937
Mads Jensen <mje.mads@gmail.com>
2016-03-07 19:51:02 +0100

    updates

--- reader_functions.py
+++ reader_functions.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 import pandas as pd
+
 from my_settings import *
 # os.chdir(tf_folder)
 
@@ -86,3 +87,92 @@ for subject in subjects:
                          "std": ctl_right_rh[:, :, 250:600].std(),
                          "max": ctl_right_rh[:, :, 250:600].max()}])
     result = result.append(row, ignore_index=True)
+
+
+X = []
+ent_left_lh = np.empty([20, 73809])
+ent_left_rh = []
+ent_right_lh = []
+ent_right_rh = []
+ctl_left_lh = []
+ctl_left_rh = []
+ctl_right_lh = []
+ctl_right_rh = []
+
+for subject in subjects[:2]:
+        # Load data
+    ent_left_lh.append(np.load("itc_%s_8-12_Brodmann.17-lh_ent_left_dSPM.npy" %
+                               subject).mean(axis=1).reshape(-1))
+    ctl_left_lh.append(np.load("itc_%s_8-12_Brodmann.17-lh_ctl_left_dSPM.npy" %
+                               subject).mean(axis=1).reshape(-1))
+    ent_right_lh.append(np.load("itc_%s_8-12_Brodmann.17-lh_ent_right_dSPM.npy"
+                                % subject).mean(axis=1).reshape(-1))
+    ctl_right_lh.append(np.load("itc_%s_8-12_Brodmann.17-lh_ctl_right_dSPM.npy"
+                                % subject).mean(axis=1).reshape(-1))
+    ent_left_rh.append(np.load("itc_%s_8-12_Brodmann.17-rh_ent_left_dSPM.npy" %
+                               subject).mean(axis=1).reshape(-1))
+    ctl_left_rh.append(np.load("itc_%s_8-12_Brodmann.17-rh_ctl_left_dSPM.npy" %
+                               subject).mean(axis=1).reshape(-1))
+    ent_right_rh.append(np.load("itc_%s_8-12_Brodmann.17-rh_ent_right_dSPM.npy"
+                                % subject).mean(axis=1).reshape(-1))
+    ctl_right_rh.append(np.load("itc_%s_8-12_Brodmann.17-rh_ctl_right_dSPM.npy"
+                                % subject).mean(axis=1).reshape(-1))
+
+ent_left_lh = np.empty([20, 73809])
+ent_left_rh = np.empty([20, 73809])
+ent_right_lh = np.empty([20, 73809])
+ent_right_rh = np.empty([20, 73809])
+ctl_left_lh = np.empty([20, 73809])
+ctl_left_rh = np.empty([20, 73809])
+ctl_right_lh = np.empty([20, 73809])
+ctl_right_rh = np.empty([20, 73809])
+
+for i, subject in enumerate(subjects[:2]):
+    tmp = np.load("itc_%s_8-12_Brodmann.17-lh_ent_left_dSPM.npy" %
+                  subject.mean(axis=1).reshape(-1)
+    ent_left_lh[i, :] = tmp
+
+    tmp = np.load("itc_%s_8-12_Brodmann.17-lh_ctl_left_dSPM.npy" %
+                  subject.mean(axis=1).reshape(-1)
+    ctl_left_lh[i, :] = tmp
+
+    tmp = np.load("itc_%s_8-12_Brodmann.17-lh_ent_right_dSPM.npy"
+                  % subject.mean(axis=1).reshape(-1)
+    ent_right_lh[i, :] = tmp
+
+    tmp = np.load("itc_%s_8-12_Brodmann.17-lh_ctl_right_dSPM.npy"
+                  % subject).mean(axis=1).reshape(-1)
+    ctl_right_lh[i, :] = tmp
+
+    tmp = np.load("itc_%s_8-12_Brodmann.17-rh_ent_left_dSPM.npy" %
+                  subject).mean(axis=1).reshape(-1)
+    ent_left_rh[i, :] = tmp
+
+    tmp = np.load("itc_%s_8-12_Brodmann.17-rh_ctl_left_dSPM.npy" %
+                  subject).mean(axis=1).reshape(-1)
+    ctl_left_rh[i,: ] = tmp
+
+    tmp = np.load("itc_%s_8-12_Brodmann.17-rh_ent_right_dSPM.npy"
+                  % subject).mean(axis=1).reshape(-1)
+    ent_right_rh[i, :] = tmp
+
+    tmp np.load("itc_%s_8-12_Brodmann.17-rh_ctl_right_dSPM.npy"
+                % subject).mean(axis=1).reshape(-1)
+    ctl_right_rh[i, :] = tmp
+
+
+ent_left_lh = np.array(ent_left_lh)
+ent_left_rh = np.array(ent_left_rh)
+ent_right_lh = np.array(ent_right_lh)
+ent_right_rh = np.array(ent_right_rh)
+ctl_left_lh = np.array(ctl_left_lh)
+ctl_left_rh = np.array(ctl_left_rh)
+ctl_right_lh = np.array(ctl_right_lh)
+ctl_right_rh = np.array(ctl_right_rh)
+
+
+X = np.vstack([ent_left_lh, ent_left_rh, ent_right_lh, ent_right_rh,
+               ctl_left_lh, ctl_left_rh, ctl_right_lh, ctl_right_rh])
+
+y = np.concatenate([[0]*20, [1]*20, [2]*20, [3]*20,
+                    [4]*20, [5]*20, [6]*20, [7]*20])
