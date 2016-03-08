077ad52d095443c97b5c4387b6674bc13a1f9c36
Mads Jensen <mje.mads@gmail.com>
2016-03-08 09:57:39 +0100

    use updates

--- extract_ts.py
+++ extract_ts.py
@@ -35,12 +35,12 @@ lbl_ent_left = []
 lbl_ctl_right = []
 lbl_ent_right = []
 
-for j in range(len(subjects)):
+for j in range(len(subjects[:1])):
     src = mne.read_source_spaces(mne_folder + "%s-oct6-src.fif" % subjects[j])
     labels = mne.read_labels_from_annot(subjects[j], parc='PALS_B12_Brodmann',
                                         regexp="Bro",
                                         subjects_dir=subjects_dir)
-    labels_occ = [labels[6], labels[7]]
+    labels_occ = [labels[6]]
 
     lbl_ent_left.append(mne.extract_label_time_course(ent_left[j],
                                                       labels=labels_occ,
@@ -67,3 +67,33 @@ lbl_ent_left = np.squeeze(np.asarray(lbl_ent_left))
 lbl_ctl_left = np.squeeze(np.asarray(lbl_ctl_left))
 lbl_ent_right = np.squeeze(np.asarray(lbl_ent_right))
 lbl_ctl_right = np.squeeze(np.asarray(lbl_ctl_right))
+
+
+
+def ITC_over_time(data, faverage=True):
+    """Calculate the ITC over time.
+
+    Parameters
+    ----------
+    data : numpy array
+        It should be trials x channels x frequencies x times.
+    faverage : bool
+        If true the average is returned, If false each frequency is returned.
+
+    Returns
+    -------
+    result : numpy array
+        The result is a numpy array with the length equal to the number of
+        trials.
+    """
+    result = np.empty([data.shape[1], data.shape[0]])
+
+    for freq in range(result.shape[0]):
+        for i in range(result.shape[1]):
+            result[freq, i] =\
+                np.abs(np.mean(np.exp(1j * (np.angle(data[i, freq, :])))))
+
+    if faverage:
+        result = result.mean(axis=0).squeeze()
+
+    return result
