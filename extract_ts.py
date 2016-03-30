6c58d07ffd0adcb282f9f908cf7d4890788bf187
Mads Jensen <mje.mads@gmail.com>
2016-03-30 10:31:39 +0200

    Change method to MNE, Fixed save

--- extract_ts.py
+++ extract_ts.py
@@ -14,7 +14,7 @@ from my_settings import *
 # Using the same inverse operator when inspecting single trials Vs. evoked
 snr = 1.0  # Standard assumption for average data but using it for single trial
 lambda2 = 1.0 / snr ** 2
-method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
+method = "MNE"  # use dSPM method (could also be MNE or sLORETA)
 freqs = np.arange(8, 13, 1)
 n_cycle = freqs / 3.
 
@@ -86,7 +86,7 @@ for subject in subjects:
             tfr = cwt_morlet(label_ts, epochs.info["sfreq"], freqs,
                              use_fft=True, n_cycles=n_cycle)
 
-            np.save(tf_folder + "%s_%s_%s-tfr" % (subject, condition,
+            np.save(tf_folder + "%s_%s_%s_MNE-tfr" % (subject, condition,
                                                     label.name),
                     tfr)
 
